262f699bbb39905b47d6801cdbb6560ef238225e
System Administrator <kilianheyne@icloud.com>
2025-06-13 07:41:21 +0200

    repo commit

---
+++ src/app/fruitlist/fruitlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fruitlist',
+  imports: [],
+  templateUrl: './fruitlist.component.html',
+  styleUrl: './fruitlist.component.scss'
+})
+export class FruitlistComponent {
+
+}
